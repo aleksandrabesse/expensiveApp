8a50ebb05cb72529e96090373baf04d01490ad67
Анна Железнякова <aleksandrabesse@gmail.com>
2020-07-12 00:01:28 +0300

    withAdd

---
+++ lib/addMenu.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AddNewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function adder;
+
+  AddNewTransaction({this.adder});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+              onPressed: () {
+                adder(title: titleController.text, value: double.parse(amountController.text));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
